21592f3de2a107f4e332afe1d31b87bf16b07926
Tbetcha <troy.boettger@gmail.com>
2020-12-06 18:22:05 -0500

    Add project files.

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace troyscar.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(troyscar.Startup))]
+namespace troyscar
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
